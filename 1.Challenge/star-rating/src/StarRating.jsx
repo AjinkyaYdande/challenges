9309dc42fc6eef4754cafe51b6e11fc434a645bc
AjinkyaYdande <37260395+AjinkyaYdande@users.noreply.github.com>
2023-09-28 23:49:47 +0530

    Challenge 1 - Star Rating in React JS

---
+++ 1.Challenge/star-rating/src/StarRating.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+const StarRating = () => {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+  return (
+    <div className="star-rating">
+      {[...Array(5)].map((star, index) => {
+        index += 1;
+        return (
+          <>
+            <button
+              key={index}
+              className={index <= rating ? "on" : "off"}
+              onClick={(e) => {
+                console.log(index, "index");
+                setRating(index);
+              }}
+              // onMouseEnter={(e) => setHover(index)}
+              // onMouseLeave={(e) => setHover(rating)}
+              onMouseEnter={() => setRating(index)}
+              onMouseLeave={() => setHover(rating - 1)}
+            >
+              <span>&#9733;</span>
+            </button>
+          </>
+        );
+      })}
+    </div>
+  );
+};
+
+export default StarRating;
